adb2c5d04df041661c9d48a92c78fdc9774f083a
Caio Mantovani Borba <caiovmborba@gmail.com>
2021-02-15 03:01:48 -0300

    Initial commit

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import {
+  Container,
+  Retweeted,
+  RocketseatIcon,
+  Body,
+  Avatar,
+  Content,
+  Header,
+  Dot,
+  Description,
+  ImageContent,
+  Icons,
+  Status,
+  CommentIcon,
+  RetweetIcon,
+  LikeIcon,
+} from './styles';
+
+const Tweet: React.FC = () => {
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Rocketseat</strong>
+            <span>@rocketseat</span>
+            <Dot />
+            <time>01 de jan</time>
+          </Header>
+
+          {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
+          <Description>Foguete não tem ré 🚀</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />9
+            </Status>
+            <Status>
+              <RetweetIcon />
+              99
+            </Status>
+            <Status>
+              <LikeIcon />
+              999
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+};
+
+export default Tweet;
